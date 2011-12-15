8268428b7b9d3abb1099d09c47b159c176ea208d
alee <zq@alee2002.com>
2011-12-15 11:31:17 +0800

    first commit

---
+++ class/DocCenter.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\doccenter ;
+
+use org\opencomb\ext\Extension ;
+
+class DocCenter extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}
