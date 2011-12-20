68324a2c72f599bb78ad9b58a0985e1a57a02271
lexdene <elephant_liu@mail.dlut.edu.cn>
2011-12-20 19:34:28 +0800

    finish generate class and function

--- class/DocumentGenerator.php
+++ class/DocumentGenerator.php
@@ -2,6 +2,9 @@
 namespace org\opencomb\doccenter ;
 
 use org\opencomb\coresystem\mvc\controller\ControlPanel ;
+use org\jecat\framework\lang\compile\CompilerFactory ;
+use org\jecat\framework\lang\compile\Compiler ;
+use org\jecat\framework\fs\FileSystem ;
 
 // /?c=org.opencomb.doccenter.DocumentGenerator&noframe=1&path[]=/framework/class/util/match/ResultSet.php&path[]=/framework/class/util/match/RegExp.php&path[]=/framework/class/util/match/Result.php
 
@@ -13,14 +16,58 @@ class DocumentGenerator  extends ControlPanel
 		$arrResult = array();
 		
 		foreach($arrPath as $path){
-			$arrResult [] = $this->GenerateByPath($path);
+			$arrResult [] = $this->generate($path);
 		}
 		echo json_encode($arrResult);
 		//
 		$this->mainView()->disable(); 
 	}
 	
-	private function GenerateByPath($path){
-		return $path;
+	private function generate($path){
+		// for debug
+		if(substr($path,-5,1) === 'r'){
+			return array(
+				'path' => $path,
+				'error'=>true,
+				'errorString' => 'for debug : '.substr($path,-5,1),
+			);
+		}
+		// for debug end
+		$arrGenerate = array();
+		$arrGenerate['path'] = $path;
+		$arrGenerate['class'] = array();
+		$arrGenerate['functions'] = array();
+		
+		$aInputStream = $this->getInputStream($path);
+		$aTokenPool = $this->getTokenPool($aInputStream);
+		foreach($aTokenPool->classIterator() as $aClassToken){
+			$classInfo = array();
+			$classInfo['name'] = $aClassToken->name();
+			$classInfo['namespace'] = $aClassToken->belongsNamespace()->name();
+			$arrGenerate['class'][] = $classInfo;
+			
+			$arrFunction = array();
+			foreach($aTokenPool->functionIterator($aClassToken->fullName()) as $aFunctionToken){
+				$functionInfo = array();
+				$functionInfo['name'] = $aFunctionToken->name();
+				$arrFunction[] = $functionInfo;
+				//echo 'function:',$aFunctionToken->name(),'<br/>';
+			}
+			$arrGenerate['functions'][$aClassToken->name()] = $arrFunction;
+		}
+		return $arrGenerate;
+	}
+	
+	private function getInputStream($path){
+		$aFile = FileSystem::singleton()->findFile($path);
+		if($aFile === null) return null;
+		return $aFile->openReader();
+	}
+	
+	private function getTokenPool($aInputStream){
+		$aCompiler = CompilerFactory::singleton()->create();
+		$aTokenPool = $aCompiler->scan($aInputStream);
+		$aCompiler->interpret($aTokenPool);
+		return $aTokenPool;
 	}
 }
