7da2849c0692182283a25b85d67ae8012203ca03
alee <zq@alee2002.com>
2012-02-01 13:19:09 +0800

    删除 Controller::createFrame() 方法，增加 Controller::defaultFrameConfig()

--- class/frame/DocFrontController.php
+++ class/frame/DocFrontController.php
@@ -1,14 +1,13 @@
 <?php
 namespace org\opencomb\doccenter\frame ;
 
-use org\opencomb\doccenter\frame\DocFrontFrame;
 use org\opencomb\coresystem\mvc\controller\Controller;
 
 class DocFrontController extends Controller
 {
-    public function createFrame()
+    public function defaultFrameConfig()
     {
-    	return new DocFrontFrame($this->params()) ;
+    	return array('class'=>'org\\opencomb\\doccenter\\frame\\DocFrontFrame') ;
     }
 }
 ?>
