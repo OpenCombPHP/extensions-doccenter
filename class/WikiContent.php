b82f06f72a6732a42e5607765aeb3362e511c1a8
kongyuan <anubiskong@gmail.com>
2012-02-09 19:21:35 +0800

    title,权限补全

--- class/WikiContent.php
+++ class/WikiContent.php
@@ -11,41 +11,41 @@ use org\jecat\framework\message\Message;
 class WikiContent extends DocFrontController {
 	public function createBeanConfig() {
 		return array (
-				'title' => '文档内容', 
-				'view:wikiContent' => array (
-						'template' => 'WikiContent.html', 
-						'class' => 'view', 
-						'model' => 'wiki' ), 
-				'model:wiki' => array (
-						'class' => 'model', 
-						'list' => true, 
-						'orm' => array (
-								'table' => 'topic', 
-								'orderAsc' => 'version',
-								'hasAndBelongsToMany:examples' => array (
-										'fromkeys' => array ('title' ), 
-										'tobridgekeys' => array ('topic_title' ), 
-										'frombridgekeys' => 'eid', 
-										'tokeys' => 'eid', 
-										'table' => 'example', 
-										'bridge' => 'example_topic', 
-										'orderAsc'=>'index'
-										) 
-								) 
-						), 
-				'model:versions' => array (
-						'class' => 'model', 
-						'list' => true, 
-						'orm' => array (
-								'table' => 'topic',
-								'orderAsc' => 'version', 
-								'columns' => array (
-										'version'
-										 ), 
-								'keys' => 'version'
-								) 
-						) 
-				);
+			'title' => '手册内容', 
+			'view:wikiContent' => array (
+					'template' => 'WikiContent.html', 
+					'class' => 'view', 
+					'model' => 'wiki' ), 
+			'model:wiki' => array (
+					'class' => 'model', 
+					'list' => true, 
+					'orm' => array (
+							'table' => 'topic', 
+							'orderAsc' => 'version',
+							'hasAndBelongsToMany:examples' => array (
+									'fromkeys' => array ('title' ), 
+									'tobridgekeys' => array ('topic_title' ), 
+									'frombridgekeys' => 'eid', 
+									'tokeys' => 'eid', 
+									'table' => 'example', 
+									'bridge' => 'example_topic', 
+									'orderAsc'=>'index'
+									) 
+							) 
+					), 
+			'model:versions' => array (
+					'class' => 'model', 
+					'list' => true, 
+					'orm' => array (
+							'table' => 'topic',
+							'orderAsc' => 'version', 
+							'columns' => array (
+									'version'
+									 ), 
+							'keys' => 'version'
+							) 
+					) 
+			);
 	}
 	
 	public function process() {
