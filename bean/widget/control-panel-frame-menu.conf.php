2eda0939e7a6b067bdcdae3bb1a15f4aba288fe4
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-02-22 11:20:50 +0800

    for rebuild of menu

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -1,2 +1,13 @@
 <?php
-return array ('development' => array ('menu' => array ('items' => array ('document-center' => array ('title' => '文档中心', 'target' => '_blank', 'link' => '?c=org.opencomb.doccenter.Index', 'query' => array ('c=org.opencomb.doccenter.Index', 'c=org.opencomb.doccenter.DocumentUpdater' ) ) ) ) ) );
+return array (
+	'item:development' => array (
+		'item:document-center' => array (
+			'title' => '文档中心',
+			'target' => '_blank',
+			'link' => '?c=org.opencomb.doccenter.Index',
+			'query' => array (
+				'c=org.opencomb.doccenter.Index',
+				'c=org.opencomb.doccenter.DocumentUpdater' )
+			)
+		)
+	);
--- class/aspect/ControlPanelFrameAspect.php
+++ class/aspect/ControlPanelFrameAspect.php
@@ -21,7 +21,9 @@ class ControlPanelFrameAspect {
 		$arrConfig = aop_call_origin ();
 		
 		// 合并配置数组，增加菜单
-		BeanFactory::mergeConfig ( $arrConfig ['frameview:frameView'] ['widget:mainMenu'] ['items'], BeanFactory::singleton ()->findConfig ( 'widget/control-panel-frame-menu', 'doccenter' ) );
+		BeanFactory::mergeConfig ( $arrConfig ['frameview:frameView'] ['widget:mainMenu'] ,
+				BeanFactory::singleton ()->findConfig ( 'widget/control-panel-frame-menu', 'doccenter' )
+		);
 		
 		return $arrConfig;
 	}
