4ca6009f103e4ce34cd7802bf72e82f20c57fc05
alee <zq@alee2002.com>
2012-04-09 14:27:15 +0800

    使用menu buildBean 事件 替代 AOP

--- class/DocCenter.php
+++ class/DocCenter.php
@@ -1,6 +1,8 @@
 <?php
 namespace org\opencomb\doccenter;
 
+use org\jecat\framework\bean\BeanFactory;
+use org\opencomb\platform\mvc\view\widget\Menu;
 use org\jecat\framework\lang\aop\AOP;
 use org\opencomb\platform\ext\Extension;
 
@@ -9,6 +11,21 @@ class DocCenter extends Extension {
 	 * 载入扩展
 	 */
 	public function load() {
-		AOP::singleton ()->register ( 'org\\opencomb\\doccenter\\aspect\\ControlPanelFrameAspect' );
+		// 注册菜单build事件的处理函数
+		Menu::registerBuildHandle(
+				'org\\opencomb\\coresystem\\mvc\\controller\\ControlPanelFrame'
+				, 'frameView'
+				, 'mainMenu'
+				, array(__CLASS__,'buildControlPanelMenu')
+		) ;
+	}
+	
+	static public function buildControlPanelMenu(array & $arrConfig)
+	{
+		// 合并配置数组，增加菜单
+		BeanFactory::mergeConfig(
+				$arrConfig
+				, BeanFactory::singleton()->findConfig('widget/control-panel-frame-menu','version-repos-tool')
+		) ;
 	}
 }
